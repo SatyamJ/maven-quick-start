17b5295943c249f0ed10c14217e3811ea866878b
Satyam Jaiswal <jaiswal.n.satyam@gmail.com>
2019-12-27 15:21:14 -0800

    java source file

---
+++ src/main/java/com/saneema/training/Application.java
@@ -0,0 +1,12 @@
+package com.saneema.training;
+
+class Application {
+    public Application() {
+        System.out.println("Inside application");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Starting Application...");
+        Application application = new Application();
+    }
+}
