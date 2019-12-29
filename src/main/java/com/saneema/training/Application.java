451a0a4415c353a377fa9238a5fdcdcf2f3cd08a
Satyam Jaiswal <snjaiswa@asu.edu>
2019-12-28 17:46:47 -0800

    changing compiler plugin for java 1.8

--- src/main/java/com/saneema/training/Application.java
+++ src/main/java/com/saneema/training/Application.java
@@ -1,12 +1,22 @@
 package com.saneema.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 class Application {
     public Application() {
         System.out.println("Inside application");
     }
 
+    private void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("greetings");
+        greetings.forEach(greeting -> System.out.println("Hello = " + greeting));
+    }
+
     public static void main(String[] args) {
         System.out.println("Starting Application...");
         Application application = new Application();
+        application.greet();
     }
 }
